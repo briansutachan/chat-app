45e15f0e8e8978719cca3d5791f0be51ee07413f
briansutachan <bsutachan@techlaunch.io>
2018-04-04 17:59:24 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,32 +1,15 @@
-const config = {
-    apiKey: "AIzaSyAe6mg772FPQgGmSCwcW_Nn5NHmat2af0w",
-    authDomain: "fir-chat-2fd1f.firebaseapp.com",
-    databaseURL: "https://fir-chat-2fd1f.firebaseio.com",
-    projectId: "fir-chat-2fd1f",
-    storageBucket: "",
-    messagingSenderId: "941893552618"
-};
-firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function (user) {
+  console.log(`user`, user);
 
-let provider = new firebase.auth.GoogleAuthProvider();
+  if (user) {
+    $(`#root`).html(ChatScreen(user));
+    chatScreenEvents(user);
 
-function googleLogin(){}
-
-firebase.auth().signInWithPopup(provider).then(function (result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-}).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+    window.user = user;
+    online(user);
+  }
+  else {
+    $('#root').append(LoginScreen());
+    loginScreenEvents();
+  }
 });
-
-$(`#google-icon`).click(googleLogin)
